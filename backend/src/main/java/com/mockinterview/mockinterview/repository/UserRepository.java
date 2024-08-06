a5e312b110eaed803b9581008bfef96ef5b6172a
Oviya.S <oviya2k05@gmail.com>
2024-08-06 15:56:58 +0530

    Revert "completion of Backend with spring security"
    This reverts commit d5b605fc514d81516aaf4042ec5dc684d2cb2c60.

--- backend/src/main/java/com/mockinterview/mockinterview/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mockinterview.mockinterview.repository;
-
-import com.mockinterview.mockinterview.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
