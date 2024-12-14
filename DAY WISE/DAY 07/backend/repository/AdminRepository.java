f972bd4fa249c86f65db8bf951f056f51d724bce
Krishnan005K <krishnankaliyappan2005@gmail.com>
2024-12-14 21:07:04 +0530

    Final Project After Removed The Unwanted Details

--- DAY WISE/DAY 07/backend/repository/AdminRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mockinterview.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mockinterview.backend.model.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, Long> {}
-
--- DAY WISE/DAY 09/backend/src/main/java/com/mockinterview/mockinterview/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mockinterview.mockinterview.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mockinterview.mockinterview.model.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, Long> {}
-
-
